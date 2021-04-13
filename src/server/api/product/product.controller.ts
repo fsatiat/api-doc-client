30dcc7714b77b95aa04b1de0ac92de5b011f70cb
Flavien Satiat <flavien.satiat@logsolution.fr>
2021-04-13 11:21:19 +0200

    feat(menu): remove product endpont

--- src/server/api/product/product.controller.ts
+++ src/server/api/product/product.controller.ts
@@ -3,24 +3,24 @@ import { ApiInternalServerErrorResponse, ApiOperation, ApiTags } from '@nestjs/s
 import { ErrorDTO } from '../core/error/error.dto';
 import { ProductDTO } from './dto/product.dto';
 
-@ApiTags('Product')
-@Controller('product')
+// @ApiTags('Product')
+// @Controller('product')
 export class ProductController {
 
-  @Get(':id')
-  @ApiOperation({ summary: 'Return detail for product' })
-  @ApiInternalServerErrorResponse({ description: 'Internal server error.', type: ErrorDTO })
-  findOne(@Param('id') id: number): ProductDTO {
-    const product: ProductDTO = {
-      id: id,
-      category: "",
-      famille: "",
-      ingredients: "",
-      name: "",
-      pictureBase64: "",
-      sousfamille: "",
-    }
+  // @Get(':id')
+  // @ApiOperation({ summary: 'Return detail for product' })
+  // @ApiInternalServerErrorResponse({ description: 'Internal server error.', type: ErrorDTO })
+  // findOne(@Param('id') id: number): ProductDTO {
+  //   const product: ProductDTO = {
+  //     id: id,
+  //     category: "",
+  //     famille: "",
+  //     ingredients: "",
+  //     name: "",
+  //     pictureBase64: "",
+  //     sousfamille: "",
+  //   }
 
-    return product;
-  }
+  //   return product;
+  // }
 }
